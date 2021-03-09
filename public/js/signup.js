5a846a675061b2aa8b14697b812d099bc9f66ccc
osteophagy <mnovakahs@gmail.com>
2021-03-08 21:52:47 -0500

    starting to integrate styling

--- public/js/signup.js
+++ public/js/signup.js
@@ -1,15 +1,15 @@
 $(document).ready(function() {
     // Getting references to our form and input
     var signUpForm = $("form.signup");
-    var emailInput = $("input#email-input");
-    var passwordInput = $("input#password-input");
+    var newEmail = $("input#new-email"); //change to newEmail throughout doc + @ IDS 
+    var newPassword = $("input#new-password"); //newPassword
   
     // When the signup button is clicked, we validate the email and password are not blank
     signUpForm.on("submit", function(event) {
       event.preventDefault();
       var userData = {
-        email: emailInput.val().trim(),
-        password: passwordInput.val().trim()
+        email: newEmail.val().trim(),
+        password: newPassword.val().trim()
       };
   
       if (!userData.email || !userData.password) {
@@ -17,8 +17,8 @@ $(document).ready(function() {
       }
       // If we have an email and password, run the signUpUser function
       signUpUser(userData.email, userData.password);
-      emailInput.val("");
-      passwordInput.val("");
+      newEmail.val("");
+      newPassword.val("");
     });
   
     // Does a post to the signup route. If successful, we are redirected to the members page
@@ -38,6 +38,44 @@ $(document).ready(function() {
     function handleLoginErr(err) {
       $("#alert .msg").text(err.responseJSON);
       $("#alert").fadeIn(500);
+    }
+
+    //========LOGIN========
+    var loginForm = $("form.login");
+    var emailInput = $("input#email-input");
+    var passwordInput = $("input#password-input");
+  
+    // When the form is submitted, we validate there's an email and password entered
+    loginForm.on("submit", function(event) {
+      event.preventDefault();
+      var userData = {
+        email: emailInput.val().trim(),
+        password: passwordInput.val().trim()
+      };
+  
+      if (!userData.email || !userData.password) {
+        return;
+      }
+  
+      // If we have an email and password we run the loginUser function and clear the form
+      loginUser(userData.email, userData.password);
+      emailInput.val("");
+      passwordInput.val("");
+    });
+  
+    // loginUser does a post to our "api/login" route and if successful, redirects us the the members page
+    function loginUser(email, password) {
+      $.post("/api/login", {
+        email: email,
+        password: password
+      })
+        .then(function() {
+          window.location.replace("/members");
+          // If there's an error, log the error
+        })
+        .catch(function(err) {
+          console.log(err);
+        });
     }
   });
   
